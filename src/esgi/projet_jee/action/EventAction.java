8fe5923b504e05b646666a71656fc6e25625bb1e
gaelgf <gael.graffion@gmail.com>
2016-07-19 15:52:19 +0200

    add Planner Events Action and Model

--- src/esgi/projet_jee/action/EventAction.java
+++ src/esgi/projet_jee/action/EventAction.java
@@ -3,8 +3,16 @@ package esgi.projet_jee.action;
 import java.util.List;
 
 
+import com.dhtmlx.planner.DHXPlanner;
+import com.dhtmlx.planner.DHXSkin;
+import com.dhtmlx.planner.controls.DHXLightboxSelect;
+import com.dhtmlx.planner.controls.DHXMiniCalendar;
+import com.dhtmlx.planner.controls.DHXUnitsView;
+import com.dhtmlx.planner.data.DHXDataFormat;
+import com.dhtmlx.planner.extensions.DHXExtension;
 import com.opensymphony.xwork2.ActionSupport;
 import esgi.projet_jee.model.User;
+import esgi.projet_jee.model.CustomEventsManager;
 import org.apache.struts2.ServletActionContext;
 import org.hibernate.Session;
 import esgi.projet_jee.util.HibernateUtil;
@@ -16,7 +24,6 @@ import javax.servlet.http.HttpSession;
 public class EventAction extends ActionSupport {
     private static final long serialVersionUID = 1L;
     private List<User> users;
-    private Boolean employee;
     private String username;
     private String userid;
     private String planner;
@@ -39,11 +46,55 @@ public class EventAction extends ActionSupport {
         return events;
     }
     public String javaplanner() throws Exception {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpSession session = request.getSession(true);
+        if (session.getAttribute("email") == null) {
+            return ERROR;
+        }
+
+        DHXPlanner planner = new DHXPlanner("./codebase/", DHXSkin.TERRACE);
+        planner.setInitialDate(2013, 7, 15);
+        planner.setInitialView("units");
+        planner.config.setScrollHour(8);
+        planner.setWidth(844);
+        planner.setHeight(518);
+        planner.load("events", DHXDataFormat.JSON);
+        planner.data.dataprocessor.setURL("events");
+        planner.config.setDetailsOnCreate(true);
+        planner.config.setFirstHour(10);
+        planner.config.setLastHour(19);
+        planner.extensions.add(DHXExtension.TOOLTIP);
+
+        // create units view
+        DHXUnitsView units = new DHXUnitsView("units", "user", "Units");
+        units.setServerListLink("users");
+        units.setSkipIncorrect(true);
+        planner.views.add(units);
+
+
+        // adds sections in lightbox
+        DHXLightboxSelect sel = new DHXLightboxSelect("user", "Owner");
+        sel.setServerList("users");
+        planner.lightbox.add(sel);
+
+
+        // creates mini-calendar
+        DHXMiniCalendar cal = new DHXMiniCalendar("minical");
+        cal.setNavigation(true);
+        planner.calendars.add(cal);
+
+        users = (new CustomEventsManager(request)).getUsers();
+        username = session.getAttribute("name").toString();
+        userid = session.getAttribute("id").toString();
+        this.planner = planner.render();
         return SUCCESS;
     }
     public String events() throws Exception {
+        CustomEventsManager evs = new CustomEventsManager(ServletActionContext.getRequest());
+        events = evs.run();
         return SUCCESS;
     }
+
     public String login() throws Exception {
         HttpServletRequest request = ServletActionContext.getRequest();
         HttpSession session = request.getSession(true);
